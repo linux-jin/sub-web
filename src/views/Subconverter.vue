d8a81b01f2ad867d3a2f3674004e8f06e9703e43
孤独的键 <aifound@88.com>
2023-04-24 13:35:38 +0800

    Update Subconverter.vue

--- src/views/Subconverter.vue
+++ src/views/Subconverter.vue
@@ -490,43 +490,48 @@ export default {
               },
               {
                 label: 'ACL4SSR 本地 默认版 分组比较全',
-                value: 'config/ACL4SSR.ini',
+                value: 'https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR.ini',
               },
               {
                 label: 'ACL4SSR_Mini 本地 精简版',
-                value: 'config/ACL4SSR_Mini.ini',
+                value: 'https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_Mini.ini',
               },
               {
                 label: 'ACL4SSR_Mini_NoAuto.ini 本地 精简版+无自动测速',
-                value: 'config/ACL4SSR_Mini_NoAuto.ini',
+                value: 'https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_Mini_NoAuto.ini',
               },
               {
                 label: 'ACL4SSR_Mini_Fallback.ini 本地 精简版+fallback',
-                value: 'config/ACL4SSR_Mini_Fallback.ini',
+                value: 'https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_Mini_Fallback.ini',
               },
               {
                 label: 'ACL4SSR_BackCN 本地 回国',
-                value: 'config/ACL4SSR_BackCN.ini',
+                value: 'https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_BackCN.ini',
               },
               {
                 label: 'ACL4SSR_NoApple 本地 无苹果分流',
-                value: 'config/ACL4SSR_NoApple.ini',
+                value: 'https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_NoApple.ini',
               },
               {
                 label: 'ACL4SSR_NoAuto 本地 无自动测速 ',
-                value: 'config/ACL4SSR_NoAuto.ini',
+                value: 'https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_NoAuto.ini',
               },
               {
                 label: 'ACL4SSR_NoAuto_NoApple 本地 无自动测速&无苹果分流',
-                value: 'config/ACL4SSR_NoAuto_NoApple.ini',
+                value: 'https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_NoAuto_NoApple.ini',
               },
               {
                 label: 'ACL4SSR_NoMicrosoft 本地 无微软分流',
-                value: 'config/ACL4SSR_NoMicrosoft.ini',
+                value: 'https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_NoMicrosoft.ini',
               },
               {
                 label: 'ACL4SSR_WithGFW 本地 GFW列表',
-                value: 'config/ACL4SSR_WithGFW.ini',
+                value: 'https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_WithGFW.ini',
+              },
+              {
+                label: 'ACL_全分组 Dream修改版',
+                value:
+                  'https://raw.githubusercontent.com/WC-Dream/ACL4SSR/master/Clash/config/ACL4SSR_Online_Full_Dream.ini',
               },
             ],
           },
@@ -682,16 +687,16 @@ export default {
                 label: 'ProxyStorage自用',
                 value: 'https://unpkg.com/proxy-script/config/Clash/clash.ini',
               },
-              {
-                label: 'ACL_全分组 Dream修改版',
-                value:
-                  'https://raw.githubusercontent.com/WC-Dream/ACL4SSR/master/Clash/config/ACL4SSR_Online_Full_Dream.ini',
-              },
+
               {
                 label: 'emby-TikTok-流媒体分组-去广告加强版',
                 value:
                   'https://raw.githubusercontent.com/justdoiting/ClashRule/main/GeneralClashRule.ini',
               },
+              {
+                 label: "流媒体通用分组",
+                 value: "https://raw.githubusercontent.com/cutethotw/ClashRule/main/GeneralClashRule.ini"
+              }
             ],
           },
           {
